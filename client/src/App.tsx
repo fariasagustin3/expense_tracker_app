e276e20d441587157b0af59b81f28b43f22d2167
fariasagustin3 <fariasagustin3@gmail.com>
2025-05-14 18:49:00 -0300

    react-router-dom added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import DashboardPage from './pages/DashboardPage'
+import TransactionsPage from './pages/TransactionsPage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<DashboardPage />} />
+      <Route path="/transactions" element={<TransactionsPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
   )
 }
 
