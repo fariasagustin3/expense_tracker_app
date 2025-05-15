b7ad9bb28a313a93230de4bf8741385e753823c8
fariasagustin3 <fariasagustin3@gmail.com>
2025-05-15 12:16:05 -0300

    login page created

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,6 +1,71 @@
-function LoginPage() {
+import { useState } from "react"
+import { LoginCredentials } from "../types/auth"
+import { Link } from "react-router-dom"
+
+const LoginPage: React.FC = () => {
+  const [credentials, setCredentials] = useState<LoginCredentials>({ 
+    email: "", 
+    password: "" 
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value 
+    })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // TODO: implement login with backend
+  }
+
   return (
-    <div>LoginPage</div>
+    <div className="w-screen h-screen px-5 py-5 bg-[#f1f1f1] flex items-center justify-center">
+      <div className="w-[400px] h-[400px] bg-white flex flex-col items-center rounded-md py-10">
+        <h1 className="text-2xl font-bold">Login</h1>
+        <form className="flex flex-col gap-5 mt-5 w-full px-16" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email">Email</label>
+            <input 
+              className="focus:outline-none border border-gray-300 px-2 py-2 rounded-sm" 
+              type="email" 
+              name="email" 
+              id="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">Password</label>
+            <input
+              className="focus:outline-none border border-gray-300 px-2 py-2 rounded-sm"
+              type="password"
+              name="password"
+              id="password"
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            className="bg-[#1d4ed8] px-2 py-2 rounded-md text-white hover:bg-[#3b82f6] transition duration-300 ease-in-out cursor-pointer"
+            type="submit"
+          >
+            Login
+          </button>
+          <hr className="w-full border border-gray-300" />
+          <p 
+            className="text-center"
+          >
+            Don't have an account? 
+            <Link 
+              to="/register"
+              className="text-[#1d4ed8] cursor-pointer"
+            >
+              {" "}Register
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
   )
 }
 
