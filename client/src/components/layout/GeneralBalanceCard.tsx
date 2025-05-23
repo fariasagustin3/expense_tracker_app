235b8ec5709d3b047786b4bab51d4f5a572567a2
fariasagustin3 <fariasagustin3@gmail.com>
2025-05-23 11:49:20 -0300

    total transactions integrated with rightbar at layout component

--- client/src/components/layout/GeneralBalanceCard.tsx
+++ client/src/components/layout/GeneralBalanceCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const GeneralBalanceCard: React.FC = () => {
+interface GeneralBalanceCardProps {
+  totalTransactions: number
+}
+
+const GeneralBalanceCard: React.FC<GeneralBalanceCardProps> = ({ totalTransactions }) => {
   return (
     <div className='w-full h-1/5 border border-gray-300 rounded-md mb-2 flex flex-col gap-2 px-4 py-6 shadow-md'>
-      <h3 className='font-semibold text-sm'>General Balance</h3>
-      <p className='font-semibold text-3xl'>$0</p>
+      <h3 className='font-semibold text-sm'>Total Transactions</h3>
+      <p className='font-semibold text-3xl'>{totalTransactions}</p>
     </div>
   )
 }
--- client/src/components/layout/Rightbar.tsx
+++ client/src/components/layout/Rightbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import GeneralBalanceCard from './GeneralBalanceCard'
 import Button from '../ui/Button'
+import { useTransactionStore } from '../../stores/useTransactionStore'
 
 const Rightbar: React.FC = () => {
+  const { transactions } = useTransactionStore((state) => state)
+
   return (
     <aside className='flex h-screen flex-col bg-white p-4 w-60 ml-auto fixed right-0 rounded-b-md'>
-      <GeneralBalanceCard />
+      <GeneralBalanceCard totalTransactions={transactions.length} />
       <Button
         label='Add Transaction'
       />
